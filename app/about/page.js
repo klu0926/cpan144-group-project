c75d2ccfbb8f8afa4e15c8bd7b0a3d9cd4b9bee0
CL <leungc22@gmail.com>
2025-07-25 23:26:40 -0400

    added card info

--- app/about/page.js
+++ app/about/page.js
@@ -3,17 +3,37 @@
 import HoverCard from "../components/HoverCard"
 
 export default function AboutPage() {
-
   return (
-    <div className="min-h-screen bg-gray-50">
-        <HoverCard 
-        name={"Jasmine Sanders"}
-        discription={"Blah blah blah"}
-        githublink={"https://github.com/Jaysandjay"}
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center py-10">
+      <h1 className="text-3xl font-bold mb-8">Meet the Team</h1>
+      <div className="flex flex-wrap justify-center gap-8">
+        <HoverCard
+          name={"Darryl Lecraw"}
+          image={"https://avatars.githubusercontent.com/u/6304758?v=4"}
+          description={"bio for Darryl"}
+          githublink={"https://github.com/BossClaw"} />
+        <HoverCard
+          name={"Clarance Leung"}
+          image={"https://avatars.githubusercontent.com/u/194448761"}
+          description={"Bio for CL"}
+          githublink={"https://github.com/kn-mn"} />
+        <HoverCard
+          name={"Kuo Yu Lu"}
+          image={"https://avatars.githubusercontent.com/u/106499794?v=4"}
+          description={"Bio for Lu"}
+          githublink={"https://github.com/klu0926"} />
+        <HoverCard
+          name={"Noah Park"}
+          image={""}
+          description={"Bio for Noah"}
+          githublink={"https://github.com/noahp211"} />
+        <HoverCard
+          name={"Jasmine Sanders"}
+          image={"https://avatars.githubusercontent.com/u/172424810?v=4"}
+          description={"Blah blah blah"}
+          githublink={"https://github.com/Jaysandja3y"}
         />
+      </div>
     </div>
-
-
-)
-
+  )
 }
